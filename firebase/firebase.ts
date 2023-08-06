31c221cd68630c34ccbcac4cf3d1f0b299a6f19a
spiritoftime <chooliangyi6@gmail.com>
2023-08-06 17:14:06 +0800

    modify gitignore to only exclude firebase json

---
+++ firebase/firebase.ts
@@ -0,0 +1,9 @@
+import admin, { ServiceAccount } from "firebase-admin";
+import serviceAccount from "./styleswap-firebase.json" assert { type: "json" };
+import dotenv from "dotenv";
+dotenv.config();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as ServiceAccount),
+  databaseURL: process.env.FIREBASE_URL,
+});
+export default admin;
